715d91abbdabd24d7cb401f3281ad332a064b528
Scott Morin <scottanthonymorin@gmail.com>
2020-09-23 15:19:52 -0400

    add m3-1 solutions

---
+++ m3-1-react--intro-and-props/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <input className='message-input' />
+    </footer>
+  );
+};
+
+export default Footer;
